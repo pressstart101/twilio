0b72da8159669066b313d73e4ee98f33f270a979
pressstart101 <walterscott404@yandex.com>
2017-11-26 20:52:18 -0500

    tea

--- app.rb
+++ app.rb
@@ -1,12 +1,39 @@
 require 'sinatra'
 require 'twilio-ruby'
 
-post '/receive_sms' do 
+post 'message' do
+  number = params["From"]
+  body = params["Body"]
   content_type 'text/xml'
 
-  response = Twilio::TwiML::Response.new do |r| 
-    #r.Message 'thanks for messaging'
-  end 
-
-  response.to_xml
+  "<Response>
+  <Message>
+  Hello #{number} you said #{body}
+  </Message>
+  </Response>"
 end
+
+
+# post '/receive_sms' do 
+#   content_type 'text/xml'
+
+#   response = Twilio::TwiML::Response.new do |r| 
+#     r.Message 'thanks for messaging'
+#   end 
+
+#   response.to_xml
+# end
+
+
+# post '/send_sms' do
+#   to = params['to']
+#   message = params['body']
+#   client = Twilio::REST::Client.new(
+#     ENV["TWILIO_ACCOUNT_SID"],
+#     ENV[TWILIO_AUTH_TOKEN])
+#   client.message.create(
+#     to: to,
+#     from: "+12345678901",
+#     body: message
+#     )
+# end
